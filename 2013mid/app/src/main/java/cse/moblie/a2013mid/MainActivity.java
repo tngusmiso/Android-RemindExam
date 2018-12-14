86cf21da579c3aa3a76df5e5785816c7d30730b5
tngusmiso <k1miso012@gmail.com>
2018-12-14 18:24:15 +0900

    2013 Midterm Exam

---
+++ 2013mid/app/src/main/java/cse/moblie/a2013mid/MainActivity.java
@@ -0,0 +1,14 @@
+package cse.moblie.a2013mid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
